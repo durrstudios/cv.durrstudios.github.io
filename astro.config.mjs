b4b3ba48c875eb92f4b7a1657f8ee9540b88f230
Durr Studios <95676114+durrstudios@users.noreply.github.com>
2023-09-05 19:48:55 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cv.durrstudios.github.io',
   integrations: [tailwind()],
   vite: {
     ssr: {
